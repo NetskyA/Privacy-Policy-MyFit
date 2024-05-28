aed625cee0d7f234c09de504d827f288b4b88de3
Aldi <aldiaafn16@gmail.com>
2024-05-28 16:16:42 +0700

    Commit All

---
+++ app_required_play_store/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      customPrimary: '#a3e635',// Medium spring green
+    },},
+  },
+  plugins: [],
+}
